6dd806cc6f96cdae3a722bbeedfc3937dc040ab8
Vojtěch Wilhelm Skyba <skymmel05@gmail.com>
2025-05-28 21:44:50 +0200

    základ

---
+++ src/component/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return (
+        <header className={"header"}>
+            <div>
+                <h1>User Management</h1>
+                <button className={"logout"}>Logout</button>
+            </div>
+        </header>
+    );
+}
